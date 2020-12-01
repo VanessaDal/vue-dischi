bc5986f5cc2b566115cbc9e467c00a8e551a34ea
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-12-01 21:58:21 +0100

    wip

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Descrizione:
+// Facciamo una chiamata ajax all’api di boolean al seguente indirizzo.
+// https://flynn.boolean.careers/exercises/api/array/music
+// L’api ci restituirà una decina di dischi musicali che dovremo stampare a schermo con Vue.
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      dischi: []
+    },
+    mounted: function () {
+      for (let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then(risposta => {
+            this.dischi.push (risposta.data.response);
+        })}
+        console.log(this.dischi)
+      }
+      //this.faiqualcosa();
+    // methods: {
+    //   generaNumero: function (indice) {
+    //     axios.get(randNUm)
+    //     .then(risposta => {
+    //       let pushedNum = risposta.data.response;
+    //       //this.numbers[0] = pushedNum;
+    //       this.$set(this.numbers, indice, pushedNum);
+    //       // console.log(pushedNum);
+    //       console.log(this.numbers);
+    //     })
+    //   },
+    //   faiqualcosa: function () {
+    //     alert("ho fatto qualcosa!");
+    //   }
+  
+    // }
+  
+  });
+  
